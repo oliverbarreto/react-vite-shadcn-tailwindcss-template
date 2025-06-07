4d36361aa6aa6132f46ea42d697cfa1a0b92ae68
Oliver Barreto <oliver.barreto.online@gmail.com>
2025-06-07 12:35:38 +0100

    INIT: Cconfiguración correccta de ESLint & Prettier en VS Code Settings:
    "editor.defaultFoldingRangeProvider": "esbenp.prettier-vscode",
      "editor.defaultFormatter": "esbenp.prettier-vscode",
      "[javascript]": { "editor.defaultFormatter": "esbenp.prettier-vscode" }

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +9,17 @@ function App() {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold text-fuchsia-950 mt-[100px]">Vite + React + Shadcn UI + Tailwind CSS Initial Templatea </h1>
-        <h3 className="text-2xl font-bold my-2">made with ❤️ by Oliver Barreto</h3>
-        <Button className="hover:bg-fuchsia-950 text-white mt-4" onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+        <h1 className="text-6xl font-bold text-fuchsia-950 mt-[100px]">
+          Vite + React + Shadcn UI + Tailwind CSS Initial Templatea{" "}
+        </h1>
+        <h3 className="text-2xl font-bold my-2">
+          made with ❤️ by Oliver Barreto
+        </h3>
+        <Button
+          className="hover:bg-fuchsia-950 text-white mt-4"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
         </Button>
       </div>
     </>
